681406b69a362d7f320b1f907c8e694e9584786f
Yang Yusheng <yang.yusheng@vpclub.cn>
2017-12-01 20:46:13 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,4 +4,10 @@
 // import './component/preview/';
 // import './component/starter';
 // import './component/stage';
-import './component/Higher/Content.jsx'
+// 高阶函数
+// import './component/Higher/Content.jsx'
+
+// redux
+// import './component/redux/preview'
+// import './component/redux/optimize'
+import './component/redux/temp'
